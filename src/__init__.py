72f7b5cb5829f45c2ea9014aff2d58fdb703f1f2
ravishankar-cloud <68008184+ravishankar-cloud@users.noreply.github.com>
2023-01-04 19:15:51 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,57 +1,60 @@
-
-from flask import Flask,redirect
 from flask.json import jsonify
-import os 
+from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
+from flask import Flask, config, redirect
+import os
 from src.auth import auth
 from src.bookmarks import bookmarks
-from src.database import db,Bookmark
-from flask_sqlalchemy import SQLAlchemy
+from src.database import db, Bookmark
 from flask_jwt_extended import JWTManager
-from src.constants.http_status_codes import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 from flasgger import Swagger, swag_from
 from src.config.swagger import template, swagger_config
 
 
-def create_app():
-    app=Flask(__name__,instance_relative_config=True)
-    app.config['FLASK_APP']="src"
-    app.config['SQLALCHEMY_DATABASE_URI']=  'sqlite:///bookmarks.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-    app.config['JWT_SECRET_KEY']='JWT_SECRET_KEY'
-    app.config['SECRET_KEY']='SECRET_KEY'
-    app.config['SWAGGER']={
-        'title': "Bookmarkss API",
-        'uiversion':3
-    }
+def create_app(test_config=None):
 
-    db.app=app
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config is None:
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get("SECRET_KEY"),
+            SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
+            SQLALCHEMY_TRACK_MODIFICATIONS=False,
+            JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY'),
+
+
+            SWAGGER={
+                'title': "Bookmarks API",
+                'uiversion': 3
+            }
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+    db.app = app
     db.init_app(app)
 
     JWTManager(app)
-    
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
-    Swagger(app,config=swagger_config,template=template)
 
-            
+    Swagger(app, config=swagger_config, template=template)
+
+    @app.get('/<short_url>')
+    @swag_from('./docs/short_url.yaml')
     def redirect_to_url(short_url):
-        bookmark=Bookmark.query.filter_by(short_url=short_url).first_or_404()
-        if bookmark:
-            bookmark.visits=bookmark.visits+1
-            db.session.commit()
+        bookmark = Bookmark.query.filter_by(short_url=short_url).first_or_404()
 
+        if bookmark:
+            bookmark.visits = bookmark.visits+1
+            db.session.commit()
             return redirect(bookmark.url)
 
     @app.errorhandler(HTTP_404_NOT_FOUND)
     def handle_404(e):
-        return jsonify({
-            'error': 'Not found'
-        }),HTTP_404_NOT_FOUND
+        return jsonify({'error': 'Not found'}), HTTP_404_NOT_FOUND
 
     @app.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
     def handle_500(e):
-        return jsonify({
-            'error': 'Something went wrong, we are working on it'
-        }),HTTP_500_INTERNAL_SERVER_ERROR
+        return jsonify({'error': 'Something went wrong, we are working on it'}), HTTP_500_INTERNAL_SERVER_ERROR
 
-    return app
+    return 
